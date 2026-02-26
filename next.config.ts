a7c1bc651908e40b8e8e3f7d242d7631f612afd3
joelmart <joel.martinez@codenation-studio.com>
2026-02-25 18:36:33 -0600

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  output: "standalone",
+  distDir: ".next"
 };
 
 export default nextConfig;
