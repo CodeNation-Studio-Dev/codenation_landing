25af1c8e7bdaa2c0c9786c40f80070ca37c1c9b9
Sandra Delgado <sandragullit@gmail.com>
2026-03-02 12:58:22 -0600

    Delete comments

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -69,47 +69,6 @@ export async function generateMetadata({
     },
   };
 }
-//   title: "CodeNation Studio | Desarrollo de Software a Medida",
-//   description:
-//     "Desarrollamos software escalable y seguro para startups y empresas en crecimiento. Desde MVP hasta sistemas empresariales, con calidad y alianza a largo plazo.",
-//   keywords: [
-//     "custom software development",
-//     "MVP development agency",
-//     "software studio Mexico",
-//     "startup technical partner",
-//     "cloud-native development",
-//     "AI integration for business",
-//     "software development Guadalajara",
-//     "scalable web applications",
-//     "product development studio",
-//   ],
-
-//   openGraph: {
-//     title: "CodeNation Studio | Desarrolo de Software a Medida",
-//     description:
-//       "Desarrollamos software escalable y seguro para startups y empresas en crecimiento. Desde MVP hasta sistemas empresariales, con calidad y alianza a largo plazo.",
-//     url: "https://codenation-studio.com/en-US",
-//     siteName: "CodeNation Studio",
-//     images: [
-//       {
-//         url: "https://codenation-studio.com/en-US/opengraph-image.png",
-//         width: 512,
-//         height: 512,
-//         alt: "CodeNation Studio isotipo",
-//       },
-//     ],
-//     locale: "en_US",
-//     type: "website",
-//   },
-
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "CodeNation Studio | Desarrollo de Software a Medida",
-//     description:
-//       "Desarrollamos software escalable y seguro para startups y empresas en crecimiento. Desde MVP hasta sistemas empresariales, con calidad y alianza a largo plazo.",
-//     images: ["https://codenation-studio.com/en-US/opengraph-image.png"],
-//   },
-// };
 
 export default async function RootLayout({
   children,
