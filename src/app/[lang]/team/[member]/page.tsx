48853d77b4e8cee76257db7d1f2ecaf008b59937
DevPacoLuna <dev.luna@outlook.com>
2026-03-11 11:40:45 -0600

    Add team routes

---
+++ src/app/[lang]/team/[member]/page.tsx
@@ -0,0 +1,33 @@
+import { Profile } from "@/src/_components/profile/Profile";
+import { getDictionary } from "@/src/_lib/helpers/getDictionary";
+import { TranslationProvider } from "@/src/_providers/translationProvider";
+
+type Members =
+  | "francisco-luna"
+  | "francisco-marmolejo"
+  | "rafael-cabrera"
+  | "joel-martinez"
+  | "aldair-gonzalez"
+  | "victor-zamora"
+  | "joshua-torres";
+
+const Page = async ({
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{
+    lang: string;
+    member: Members;
+  }>;
+}>) => {
+  const { lang, member } = await params;
+  const dict = await getDictionary(lang, "members");
+
+  return (
+    <TranslationProvider dict={dict}>
+      <Profile user={member} />
+    </TranslationProvider>
+  );
+};
+
+export default Page;
