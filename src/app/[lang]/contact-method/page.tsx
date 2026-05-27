e75b3b75ede33494a1c40d5b18e1656e90039916
Sandra Delgado <sandragullit@gmail.com>
2026-05-27 13:08:03 -0600

    Add contact us and contact method components

--- src/app/[lang]/contact-method/page.tsx
+++ src/app/[lang]/contact-method/page.tsx
@@ -2,7 +2,7 @@ import CallOrMail from "@/src/_components/contactUs/CallOrMail";
 
 const Page = () => {
   return (
-    <div className="from-background via-primary/15 to-background flex min-h-[80vh] w-full items-center justify-center bg-gradient-to-t">
+    <div className="from-background via-primary/15 to-background flex min-h-[100vh] w-full items-center justify-center bg-gradient-to-t md:h-[calc(100vh-82px)] md:min-h-auto">
       <CallOrMail />
     </div>
   );
